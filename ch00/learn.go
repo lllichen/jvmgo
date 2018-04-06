b37e8fb18da8dc0d8b804fd451b40900d2e41636
Li Chen <lichen@teleinfo.cn>
2018-04-06 21:20:46 +0800

    add info

---
+++ ch00/learn.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//11001010
+	var t byte= 202
+	fmt.Printf("%v\n",t)
+	fmt.Printf("%b\n",t)
+	fmt.Printf("%d\n",t)
+	fmt.Printf("%x\n",t)
+	fmt.Printf("%d\n",t)
+	fmt.Printf("%d\n",t)
+
+
+	if t == 0Xca{
+		fmt.Printf("ok")
+	}
+	fmt.Println("hello")
+}
