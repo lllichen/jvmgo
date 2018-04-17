94ddd2e2eee88340aef6aa0f642aed561eb56fdc
Li Chen <lichen@teleinfo.cn>
2018-04-17 09:22:47 +0800

    add instruction

--- ch05/rtda/frame.go
+++ ch05/rtda/frame.go
@@ -4,6 +4,8 @@ type Frame struct {
 	lower *Frame
 	localVars LocalVars
 	operandStack *OperandStack
+	thread       *Thread
+	nextPC       int // the next instruction after the call
 }
 
 func (frame *Frame) LocalVars() LocalVars {
@@ -12,11 +14,11 @@ func (frame *Frame) LocalVars() LocalVars {
 func (frame *Frame) OperandStack() OperandStack {
 	return *frame.operandStack
 }
-func (frame *Frame) Thread() {
-	
+func (frame *Frame) Thread() *Thread {
+	return frame.thread
 }
-func (frame *Frame) SetNextPC(nextPc interface{}) {
-	
+func (frame *Frame) SetNextPC(nextPc int) {
+	frame.nextPC = nextPc
 }
 
 func NewFrame(maxLocals, maxStack uint) *Frame{
--- ch05/rtda/thread.go
+++ ch05/rtda/thread.go
@@ -24,3 +24,6 @@ func (thread *Thread) PopFrame( ) *Frame{
 func (thread *Thread) CurrentFrame( ) *Frame{
 	return thread.stack.top()
 }
+func (thread *Thread) PC() int {
+	return thread.pc
+}
