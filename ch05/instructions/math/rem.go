3bffb882083942812ef472111852aec861ab2161
Li Chen <lichen@teleinfo.cn>
2018-04-19 08:26:08 +0800

    add

--- ch05/instructions/math/rem.go
+++ ch05/instructions/math/rem.go
@@ -22,6 +22,15 @@ type FREM struct {
 	base.NoOperandsInstruction
 }
 
+
+func (frem *FREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopFloat()
+	v1 := stack.PopFloat()
+	result := float32(math.Mod(float64(v1), float64(v2)))
+	stack.PushFloat(result)
+}
+
 type IREM struct {
 	base.NoOperandsInstruction
 }
@@ -41,3 +50,13 @@ type LREM struct {
 	base.NoOperandsInstruction
 }
 
+func (lrem *LREM) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopLong()
+	v1 := stack.PopLong()
+	if v2 == 0 {
+		panic("java.lang.ArithmeticException:/ by zero")
+	}
+	result := v1%v2
+	stack.PushLong(result)
+}
