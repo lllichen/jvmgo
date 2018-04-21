67b63aa3a57d22fbba20254660139251b076d99c
Li Chen <lichen@teleinfo.cn>
2018-04-21 08:20:04 +0800

    add sh code

---
+++ book/ch05/GaussTest.java
@@ -0,0 +1,14 @@
+package jvm.book.ch05
+
+
+public class GaussTest {
+
+
+    public static void main (String[] args){
+        int sum = 0;
+        for (int i = 0; i <=100 ; i++){
+            sum +=i;
+        }
+        System.out.Println(sum)
+    }
+}
--- ch05/main.go
+++ ch05/main.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 import (
-	"jvmgo/ch05/rtda"
+	"jvmgo/ch05/classpath"
+	"strings"
+	"jvmgo/ch05/classfile"
 )
 
 func main() {
@@ -18,11 +20,11 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	//cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
 	//fmt.Printf("classpath:%v class:%v args:%v\n",
 	//	cp, cmd.class, cmd.args)
 	//
-	//className := strings.Replace(cmd.class, ".", "/", -1)
+	className := strings.Replace(cmd.class, ".", "/", -1)
 	//classData, _, err := cp.ReadClass(className)
 	//if err != nil {
 	//	fmt.Printf("Could not find or load main class %s\n", cmd.class)
@@ -31,66 +33,34 @@ func startJVM(cmd *Cmd) {
 	////print data
 	//fmt.Printf("class data:%v\n", classData)
 	//
-	//cf := loadClass(className,cp)
-	//fmt.Println(cmd.class)
-	//printClassInfo(cf)
-	//fmt.Println(cf.MajorVersion())
-	//fmt.Println(cf.AccessFlags())
-	//fmt.Println(cf.ThisClass())
-	//fmt.Println(cf.SuperClass())
-	//fmt.Println(cf.ConstantPool())
-
-	frame := rtda.NewFrame(100,100)
-	testLocalVars(frame.LocalVars())
-	testOperandVars(frame.OperandStack())
-}
-
-
-func testLocalVars(vars rtda.LocalVars){
-	vars.SetInt(0,100)
-	vars.SetInt(1,-100)
-
-	vars.SetLong(2,2997924580)
-	vars.SetLong(4,-2997924580)
-
-	vars.SetFloat(6,3.1415926)
-	vars.SetDouble(7,2.71828182845)
-	vars.SetRef(9,nil)
-
-	println(vars.GetInt(0))
-	println(vars.GetInt(1))
-	println(vars.GetLong(2))
-	println(vars.GetLong(4))
-	println(vars.GetFloat(6))
-	println(vars.GetDouble(7))
-	println(vars.GetRef(9))
-
-}
-
-
-
-func testOperandVars(vars rtda.OperandStack){
-	vars.PushInt(100)
-	vars.PushInt(-100)
-
-	vars.PushLong(2997924580)
-	vars.PushLong(-2997924580)
-
-	vars.PushFloat(3.1415926)
-	vars.PushDouble(2.71828182845)
-	vars.PushRef(nil)
-
-	println(vars.PopRef())
-	println(vars.PopDouble())
-	println(vars.PopFloat())
-	println(vars.PopLong())
-	println(vars.PopLong())
-	println(vars.PopInt())
-	println(vars.PopInt())
-
-
-
-
+	cf := loadClass(className,cp)
+	mainMethod := getMainMethod(cf)
+	if mainMethod != nil {
+		interpret(mainMethod)
+	}else {
+		fmt.Printf("Main method not found in class %s\n",cmd.class)
+	}
 
 
 }
+func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
+	classData,_, err := cp.ReadClass(className)
+	if err != nil {
+		panic(err)
+	}
+	cf, err := classfile.Parse(classData)
+	if err != nil {
+		panic(err)
+	}
+	return cf
+}
+
+func getMainMethod(cf *classfile.ClassFile) *classfile.MemberInfo{
+	for _,m := range cf.Methods(){
+		if m.Name() == "main" && m.Descriptor() == "[Ljava/lang/String;)V" {
+			return m
+		}
+	}
+	return nil
+}
+
