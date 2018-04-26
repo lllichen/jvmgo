a80bbbc1ae813b7dec003c30c703fb0488297bcc
Li Chen <lichen@teleinfo.cn>
2018-04-27 07:22:33 +0800

    add all

--- ch06/rtda/frame.go
+++ ch06/rtda/frame.go
@@ -8,25 +8,28 @@ type Frame struct {
 	nextPC       int // the next instruction after the call
 }
 
-func (frame *Frame) LocalVars() LocalVars {
-	return frame.localVars
-}
-func (frame *Frame) OperandStack() OperandStack {
-	return *frame.operandStack
-}
-func (frame *Frame) Thread() *Thread {
-	return frame.thread
-}
-func (frame *Frame) SetNextPC(nextPc int) {
-	frame.nextPC = nextPc
-}
-func (frame *Frame) NextPc() int {
-	return frame.nextPC
-}
 
 func newFrame(thread *Thread, maxLocals, maxStack uint) *Frame{
 	return &Frame{
 		thread:thread,
 		localVars: newLocalVars(maxLocals),
-	operandStack:newOperandStack(maxStack)}
+		operandStack:newOperandStack(maxStack)}
+}
+
+func (frame *Frame) LocalVars() LocalVars {
+	return frame.localVars
+}
+func (frame *Frame) OperandStack() *OperandStack {
+	return frame.operandStack
+}
+func (frame *Frame) Thread() *Thread {
+	return frame.thread
+}
+
+func (frame *Frame) NextPC() int {
+	return frame.nextPC
+}
+
+func (frame *Frame) SetNextPC(nextPc int) {
+	frame.nextPC = nextPc
 }
