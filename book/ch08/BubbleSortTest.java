f40b9c7cc5d39a5e91e0352ba5f0b9be18f1bb84
Li Chen <lichen@teleinfo.cn>
2018-06-21 08:17:37 +0800

    add all info

--- book/ch08/BubbleSortTest.java
+++ book/ch08/BubbleSortTest.java
@@ -1,4 +1,4 @@
-package jvmgo.book.ch08
+package jvmgo.book.ch08;
 
 
 public class BubbleSortTest {
@@ -18,7 +18,7 @@ public class BubbleSortTest {
             while(swapped){
                 swapped = false;
                 j++;
-                for (int i=0; i < arr.length - j; j++) {
+                for (int i=0; i < arr.length - j; i++) {
                     if (arr[i] > arr[i+1]) {
                         tmp = arr[i];
                         arr[i] = arr[i+1];
