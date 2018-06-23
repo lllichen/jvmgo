24dee664f4ae17115f0bdb02fff06082338366c7
Li Chen <lichen@teleinfo.cn>
2018-06-23 12:08:45 +0800

    fix bug

--- ch08/instructions/comparisons/if_icmp.go
+++ ch08/instructions/comparisons/if_icmp.go
@@ -8,6 +8,7 @@ import (
 type IF_ICMPEQ struct {base.BranchInstruction}
 
 func (ifIcmpeq *IF_ICMPEQ) Execute(frame *rtda.Frame) {
+
 	stack := frame.OperandStack()
 	v2 := stack.PopInt()
 	v1 := stack.PopInt()
@@ -67,8 +68,9 @@ func (ifIcmpge *IF_ICMPGE) Execute(frame *rtda.Frame) {
 	stack := frame.OperandStack()
 	v2 := stack.PopInt()
 	v1 := stack.PopInt()
-	if v1 > v2 {
+	if v1 >= v2 {
 		base.Branch(frame,ifIcmpge.Offset)
 	}
 }
+
 
