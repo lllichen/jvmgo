b444cb5f83aa7b177617b06ee4359c9e2eb4cd2e
Li Chen <lichen@teleinfo.cn>
2018-06-27 07:54:40 +0800

    add native instrution

---
+++ ch09/instructions/reserved/invokenative.go
@@ -0,0 +1,30 @@
+package reserved
+
+import (
+	"jvmgo/ch09/instructions/base"
+	"jvmgo/ch09/rtda"
+	"jvmgo/ch09/native"
+)
+
+type INVOKE_NATIVE struct {
+	base.NoOperandsInstruction
+}
+
+func (invokeNative *INVOKE_NATIVE) Execute(frame *rtda.Frame) {
+	method := frame.Method()
+	className := method.Class().Name()
+	methodName := method.Name()
+	methodDescriptor := method.Descriptor()
+
+	nativeMethod := native.FindNativeMethod(className, methodName, methodDescriptor)
+
+	if nativeMethod == nil {
+		methodinfo := className + "." + methodName + methodDescriptor
+		panic("java.lang.UnsatisfiedLinkError: " + methodinfo)
+	}
+
+	nativeMethod(frame)
+}
+
+
+
