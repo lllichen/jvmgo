b4a4fe4ab288d08e156551736bb9ef77bdca8f10
Li Chen <lichen@teleinfo.cn>
2018-07-05 22:53:03 +0800

    add clone

--- ch09/native/java/lang/Object.go
+++ ch09/native/java/lang/Object.go
@@ -6,9 +6,13 @@ import (
 	"unsafe"
 )
 
+const jlObject  = "java/lang/Object"
+
 func init(){
-	native.Register("java/lang/Object","getClass","()Ljava/lang/Class;",getClass)
-	native.Register("java/lang/Object", "hashCode", "()I", hashCode)
+
+	native.Register(jlObject,"getClass","()Ljava/lang/Class;",getClass)
+	native.Register(jlObject, "hashCode", "()I", hashCode)
+	native.Register(jlObject, "clone", "()Ljava/lang/Object;", clone)
 }
 
 func getClass(frame *rtda.Frame){
@@ -21,4 +25,15 @@ func hashCode(frame *rtda.Frame) {
 	this := frame.LocalVars().GetThis()
 	hash := int32(uintptr(unsafe.Pointer(this)))
 	frame.OperandStack().PushInt(hash)
+}
+
+func clone(frame *rtda.Frame) {
+	this := frame.LocalVars().GetThis()
+
+	cloneable := this.Class().Loader().LoadClass("java/lang/Cloneable")
+
+	if !this.Class().IsImplements(cloneable) {
+		panic("java.lang.CloneNotSupportedException")
+	}
+	frame.OperandStack().PushRef(this.Clone())
 }
